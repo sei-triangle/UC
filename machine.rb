ef3ba0d067bb9818626ee4411a853d9080536ca8
sei-triangle <sei-triangle@outlook.com>
2015-02-07 13:47:03 +0900

    first commit

---
+++ machine.rb
@@ -0,0 +1,16 @@
+require './simple.rb'
+
+class Machine < Struct.new(:expression)
+  def step
+    self.expression = expression.reduce
+  end
+
+  def run
+    while expression.reducible?
+      puts expression
+      step
+    end
+    puts expression
+  end
+end
+
