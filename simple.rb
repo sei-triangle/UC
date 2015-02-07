268ae0e1401b2d890736ded91e3df90aca4990b7
sei-triangle <sei-triangle@outlook.com>
2015-02-07 14:11:09 +0900

    Machine, Boolean, LessThan

--- simple.rb
+++ simple.rb
@@ -60,3 +60,41 @@ class Multiply < Struct.new(:left, :right)
   end
 end
 
+class Boolean < Struct.new(:value)
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "<#{self}>"
+  end
+
+  def reducible?
+    false
+  end
+end
+
+class LessThan < Struct.new(:left, :right)
+  def to_s
+    "#{left} < #{right}"
+  end
+
+  def inspect
+    "<#{$self}>"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      LessThan.new(left.reduce, right)
+    elsif right.reducible?
+      LessThan.new(left, right.reduce)
+    else
+      Boolean.new(left.value < right.value)
+    end
+  end
+end
+
