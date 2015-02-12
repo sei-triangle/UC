6101b7132154232965a9961f0233a7b24d21ffdc
seimiya <h_seimiya@skymark.jp>
2015-02-12 16:27:59 +0900

    Completion denotational

--- denotational/statement.rb
+++ denotational/statement.rb
@@ -26,4 +26,40 @@ class If < Struct.new(:condition, :consequence, :alternative)
   def to_s
     "if (#{condition}) { #{consequence} } else { #{alternative} }"
   end
+  def inspect
+    "<#{self}>"
+  end
+  def to_ruby
+    "-> e { if {#{condition.to_ruby}).call(e)" +
+      " then {#{consequence.to_ryby}).call(e)" +
+      " else {#{alternative.to_ruby}).call(e)" +
+      " end}"
+  end
+end
+
+class Sequence < Struct.new(:first, :second)
+  def to_s
+    "#{first}; #{second}"
+  end
+  def inspect
+    "<#{self}>"
+  end
+  def to_ruby
+    "-> e { (#{second.to_ruby}).call((#{first.to_ruby}).call(e)) }"
+  end
+end
+
+class While < Struct.new(:condition, :body)
+  def to_s
+    "while (#{condition}) { #{body} }"
+  end
+  def inspect
+    "<#{self}>"
+  end
+  def to_ruby
+    "-> e {" +
+      " while (#{condition.to_ruby}).call(e); e = (#{body.to_ruby}).call(e); end;" +
+      " e" +
+      " }"
+  end
 end
